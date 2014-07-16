06c188e591088f278279c983576fcdc10ddc4442
Andrew Betts <andrew.betts@ft.com>
2014-07-16 11:26:01 +0100

    Remove resize code, now handled by embedapi from the registry

--- demos/src/declarative.js
+++ demos/src/declarative.js
@@ -3,19 +3,4 @@
 require('./polyfills.js');
 
 var Gallery = require('./../../main.js');
-
-if (document.addEventListener) {
-    document.addEventListener("oGallery.ready", function (evt) {
-        "use strict";
-        if (typeof console === "object" && typeof console.log === "function") {
-            console.log("Gallery ready", evt.detail.gallery);
-            parent && parent.postMessage(JSON.stringify({
-                type: 'resize',
-                url: location.href,
-                height: document.body.scrollHeight
-            }), '*');
-        }
-    });
-}
-
 window.galleries = Gallery.createAllIn(document.body);
--- demos/src/imperative.js
+++ demos/src/imperative.js
@@ -4,20 +4,6 @@ require('./polyfills.js');
 
 var Gallery = require('./../../main.js');
 
-if (document.addEventListener) {
-    document.addEventListener("oGallery.ready", function (evt) {
-        "use strict";
-        if (typeof console === "object" && typeof console.log === "function") {
-            console.log("Gallery ready", evt.detail.gallery);
-            parent && parent.postMessage(JSON.stringify({
-                type: 'resize',
-                url: location.href,
-                height: document.body.scrollHeight
-            }), '*');
-        }
-    });
-}
-
 var slideshowImperative = new Gallery(document.getElementById("imperative-slideshow"), slideshowGalleryConfig),
     thumbnailImperative = new Gallery(document.getElementById("imperative-thumbnails"), thumbnailGalleryConfig);
 
